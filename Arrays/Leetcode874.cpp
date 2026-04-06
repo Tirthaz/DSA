41ecbe0029be0fc89a31bff5282b35c1aaa66323
Tirtha Zoldeo <135409451+Tirthaz@users.noreply.github.com>
2026-04-06 23:16:30 +0530

    Implement robot simulation with obstacle handling

---
+++ Arrays/Leetcode874.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+      
+        unordered_set<long long> obs;
+        
+        for (auto &o : obstacles) {
+            long long key = ((long long)o[0] << 32) | (o[1] & 0xffffffff);
+            obs.insert(key);
+        }
+        
+        vector<pair<int,int>> dir = {
+            {0,1}, {1,0}, {0,-1}, {-1,0}
+        };
+        
+        int d = 0;
+        int x = 0, y = 0;
+        int maxDist = 0;
+        
+        for (int cmd : commands) {
+            
+            if (cmd == -1) {
+                d = (d + 1) % 4; 
+            }
+            else if (cmd == -2) {
+                d = (d + 3) % 4; 
+            }
+            else {
+                for (int i = 0; i < cmd; i++) {
+                    
+                    int nx = x + dir[d].first;
+                    int ny = y + dir[d].second;
+                    
+                    long long key = ((long long)nx << 32) | (ny & 0xffffffff);
+                    
+                    if (obs.count(key)) break;
+                    
+                    x = nx;
+                    y = ny;
+                    
+                    maxDist = max(maxDist, x*x + y*y);
+                }
+            }
+        }
+        
+        return maxDist;
+    }
+};
