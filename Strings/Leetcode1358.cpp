11d1cff9ff8f41e4fd5b366f7d17101fca4e12e6
Tirtha Zoldeo <135409451+Tirthaz@users.noreply.github.com>
2026-06-30 21:51:09 +0530

    number of substrings containing all three characters

---
+++ Strings/Leetcode1358.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int lastA = -1, lastB = -1, lastC = -1;
+        int ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'a')
+                lastA = i;
+            else if (s[i] == 'b')
+                lastB = i;
+            else
+                lastC = i;
+
+            ans += min({lastA, lastB, lastC}) + 1;
+        }
+
+        return ans;
+    }
+};
